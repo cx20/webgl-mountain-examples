f88a103d3d196af851b69e4f82a5466c967f824f
cx20 <y-shimano@hondarer-soft.com>
2019-07-24 07:16:16 +0900

    Modified to use ES6 syntax

--- jsdo.it/cx20/i7Lt/index.js
+++ jsdo.it/cx20/i7Lt/index.js
@@ -1,28 +1,28 @@
-﻿var csv2img = function(text, height_max){
-	//var s = text.split("\n"); // CSV を配列に展開
+﻿let csv2img = function(text, height_max){
+	//let s = text.split("\n"); // CSV を配列に展開
 	//s = s.filter(function(e) { return e !== ""; }); // 空行は除外
-    var s = text.split(",");
+    let s = text.split(",");
 
-	var WIDTH = 257;
-	var HEIGHT = 257;
+	let WIDTH = 257;
+	let HEIGHT = 257;
 
-	var canvas = document.createElement('canvas');
+	let canvas = document.createElement('canvas');
 	canvas.width = WIDTH-1;
 	canvas.height = HEIGHT-1;
-	var ctx = canvas.getContext("2d");
+	let ctx = canvas.getContext("2d");
 
-	var img = ctx.createImageData(WIDTH, HEIGHT);
-	var buffer = img.data;
-	var x, y, z;
-	var data = [];
+	let img = ctx.createImageData(WIDTH, HEIGHT);
+	let buffer = img.data;
+	let x, y, z;
+	let data = [];
 	// HeightMap 作成処理
-	for (var i = 0; i < HEIGHT-1; i++) {
-		for (var j = 0; j < WIDTH-1; j++) {
-			var h = s[i*WIDTH+j];
+	for (let i = 0; i < HEIGHT-1; i++) {
+		for (let j = 0; j < WIDTH-1; j++) {
+			let h = s[i*WIDTH+j];
 			x = j;
 			y = i;
 			z = Math.floor((h / height_max) * 256); // 256諧調
-			var pos = (y * HEIGHT + x) * 4;
+			let pos = (y * HEIGHT + x) * 4;
 			buffer[pos + 0] = z;
 			buffer[pos + 1] = z;
 			buffer[pos + 2] = z;
@@ -31,7 +31,7 @@
 	}
 	
 	ctx.putImageData(img, 0, 0);
-	var result = canvas.toDataURL();
+	let result = canvas.toDataURL();
 	
 	return result;
 };
@@ -42,10 +42,10 @@ window.addEventListener('DOMContentLoaded', function() {
 		// File APIを利用できるかをチェック
 		if (window.File) {
 			// 指定されたファイルを取得
-			var input = document.querySelector('#file').files[0];
-			var height_max = document.getElementById('height_max').value;
+			let input = document.querySelector('#file').files[0];
+			let height_max = document.getElementById('height_max').value;
 			// ファイル読み込みの準備（1）
-			var reader = new FileReader();
+			let reader = new FileReader();
 			// ファイルの読み込みに成功したら、その内容を<div id="result">に反映（2）
 			reader.addEventListener('load', function(e) {
 				document.getElementById("img1").src = csv2img(reader.result, height_max);
